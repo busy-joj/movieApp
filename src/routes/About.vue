be6e5c2a781a1ba34dc2ffe7dc4dcae9e5178f93
busy-joj <busy.joj@gmail.com>
2022-07-13 16:48:23 +0900

    about 컴포넌트 작업 완료

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,60 @@
 <template>
-  <h1>about</h1>
+  <div class="about">
+    <div class="photo">
+      <Loader v-if="imageLoading" absolute/>
+      <img :src="image" alt="">
+    </div>
+    <div class="name">{{ name }}</div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+  </div>
 </template>
+
+<script>
+import Loader from '~/components/Loader'
+
+export default {
+  data(){
+    return{
+      imageLoading:true
+    }
+  },
+  computed :{
+    image(){
+      return this.$store.state.about.image
+    },
+    name(){
+      return this.$store.state.about.name  
+    },
+    email(){
+      return this.$store.state.about.email  
+    },
+    blog(){
+      return this.$store.state.about.blog  
+    }
+  },
+  mounted(){
+    this.init()
+  },
+  methods:{
+    async init(){
+      await this.$loadImage(this.image)
+      this.imageLoading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.about{text-align: center;padding-top:40px;font-size:20px;
+  .photo{
+    width:300px;height:300px;margin:40px auto 20px;border:10px solid $gray-300;border-radius:50%;box-sizing: border-box;background-color:$gray-200;position:relative;
+    img{width:280px;border-radius:50%;}
+  }
+  .name{
+    font-size:50px;font-weight:bold;font-family: 'Oswald', sans-serif;margin-bottom:20px;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default{
+    namespaced: true,
+    state: () => ({
+        name: 'joj',
+        email: 'busy-joj@gmail.com',
+        blog: 'https://busy-joj.github.io/',
+        image: 'https://avatars.githubusercontent.com/u/97998975?s=400&u=868c773d9098497f023b76289c34920c3ed7b0c2&v=4'
+    })
+    
+}
